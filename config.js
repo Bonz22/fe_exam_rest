51ca81a9248b8473c355bdfb7caeb4a71860c995
Bonifacio L Del Rosario Jr <bldelrosario@sysnetph.com>
2021-12-15 20:07:06 +0800

    First Commit - REST API sample

---
+++ config.js
@@ -0,0 +1,21 @@
+const config = {
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: '4332wurx',
+        database: 'fe_exam'
+    },
+    server: {
+        port: 3000,
+        view: 'ejs'
+    },
+    session: {
+        secret: '1234',
+        resave: false,
+        saveUninitialized: false,
+        expires: 600000,
+        maxAge: 3600000
+    }
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,136 @@
+const config = require('./config');
+const express = require('express');
+const Promise = require('promise');
+const mysql = require('mysql');
+const pool = mysql.createPool(config.mysql);
+const app = express();
+const http = require('http').Server(app);
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+let getRecords = () => {
+	return new Promise((resolve, reject)=>{
+		pool.getConnection(function(err,connection){
+			if(err){
+				return reject(err);
+			};
+			connection.query('SELECT * FROM calendar',(error,result)=>{
+				connection.release();
+				if(error){
+					console.log({error});
+					return reject(error);
+				};
+				resolve(result);
+			});
+		});
+	});
+};
+
+let updateRecords = (req) => {
+	return new Promise((resolve, reject)=>{
+		pool.getConnection(function(err,connection){
+			if(err){
+				return reject(err);
+			};
+			let q = `UPDATE calendar SET event = ?, date_created = ? WHERE id = ?`;
+			connection.query(q, [req.body.event, req.body.date, req.body.id],(error,result)=>{
+				connection.release();
+				if(error){
+					console.log({error});
+					return reject(error);
+				};
+				resolve(result);
+			});
+		});
+	});
+};
+
+let deleteRecords = (id) => {
+	return new Promise((resolve, reject)=>{
+		pool.getConnection(function(err,connection){
+			if(err){
+				return reject(err);
+			};
+			let q = `DELETE FROM fe_exam.calendar WHERE id = ?`;
+			connection.query(q, id,(error,result)=>{
+				connection.release();
+				if(error){
+					console.log({error});
+					return reject(error);
+				};
+				resolve(result);
+			});
+		});
+	});
+};
+
+let createRecord = (req) => {
+	return new Promise((resolve, reject)=>{
+		pool.getConnection(function(err,connection){
+			if(err){
+				return reject(err);
+			};
+			let q = `INSERT INTO fe_exam.calendar(event, date_created) VALUES (?, ?) `;
+			connection.query(q, [req.body.event, req.body.date],(error,result)=>{
+				connection.release();
+				if(error){
+					console.log({error});
+					return reject(error);
+				};
+				resolve(result);
+			});
+		});
+	});
+};
+
+app.get('/get',(req,res)=>{
+	getRecords().then((result)=>{
+		res.json(result);
+	})
+	.catch((error)=>{
+		res.json(error);
+	});
+});
+
+app.post('/update',(req,res)=>{
+	updateRecords(req).then((result)=>{
+		//console.log({result});
+		res.json({
+			status: (result.affectedRows > 0) ? 'Update Success' : 'Update Failed'
+		});
+	})
+	.catch((error)=>{
+		res.json(error);
+	});
+});
+
+app.delete('/delete/:id',(req,res)=>{
+	deleteRecords(req.params.id).then((result)=>{
+		res.json({
+			status: (result.affectedRows > 0) ? 'Record Deletion Successful' : 'Record Deletion Failed'
+		});
+	})
+	.catch((error)=>{
+		res.json(error);
+	});
+});
+
+app.put('/create',(req,res)=>{
+	createRecord(req).then((result)=>{
+		res.json({
+			status: (result.affectedRows > 0) ? 'Record Creation Successful' : 'Record Creation Failed'
+		});
+	})
+	.catch((error)=>{
+		res.json(error);
+	});
+});
+
+app.use((req,res,next)=>{
+    res.status(404).render('error',{title: 'Page Not Found!', header_error: 'Oops!', error_message: "We can't seem to find the page you're looking for"});
+});
+
+http.listen(config.server.port,()=>{
+    console.log('Server running on port ' + config.server.port);
+});
